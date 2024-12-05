c98aeeafaa44cca0df62661a38680106dc988ea1
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-12-05 02:44:07 +0100

    add code for time scaling

--- src/time_manager.h
+++ src/time_manager.h
@@ -5,14 +5,7 @@
 struct SearchInfo;
 struct ThreadData;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-    void Optimum(struct SearchInfo* info, int time, int inc);
-    bool StopEarly(const struct SearchInfo* info);
-    bool TimeOver(const struct SearchInfo* info);
-
-#ifdef __cplusplus
-}
-#endif
+void Optimum(struct SearchInfo* info, int time, int inc);
+bool StopEarly(const struct SearchInfo* info);
+bool TimeOver(const struct SearchInfo* info);
+void ScaleTm(ThreadData* td);
