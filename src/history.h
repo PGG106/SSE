50a205761e66f83a3b72b0e7c8c95a72d2f715f0
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-12-05 23:06:18 +0100

    cleanup

--- src/history.h
+++ src/history.h
@@ -17,31 +17,25 @@ const static int CORRHIST_GRAIN = 256;
 #define CORRHIST_SIZE 16384
 const static int CORRHIST_MAX = 16384;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-    // Functions used to update the history heuristics
-    void UpdateHistories(const struct Position* pos, struct SearchData* sd, struct SearchStack* ss, const int depth, const Move bestMove, const struct MoveList* quietMoves, const struct MoveList* noisyMoves, const bool rootNode);
+// Functions used to update the history heuristics
+void UpdateHistories(const struct Position* pos, struct SearchData* sd, struct SearchStack* ss, const int depth, const Move bestMove, const struct MoveList* quietMoves, const struct MoveList* noisyMoves, const bool rootNode);
 
-    // Fuction that returns the history bonus
-    int history_bonus(const int depth);
+// Fuction that returns the history bonus
+int history_bonus(const int depth);
 
-    // Getters for the history heuristics
-    int GetHHScore(const struct Position* pos, const struct SearchData* sd, const Move move);
-    int GetCHScore(const struct SearchStack* ss, const Move move);
-    int GetSingleCHScore(const struct SearchStack* ss, const Move move, const int offset);
-    int GetCapthistScore(const struct Position* pos, const struct SearchData* sd, const Move move);
-    int GetHistoryScore(const struct Position* pos, const struct SearchData* sd, const Move move, const struct SearchStack* ss);
-    // Clean all the history tables
-    void CleanHistories(struct SearchData* sd);
-    // Updates history heuristics for a single move
-    void updateHHScore(const struct Position* pos, struct SearchData* sd, const Move move, int bonus);
-    void updateCHScore(struct SearchStack* ss, const Move move, const int bonus);
-    void updateCapthistScore(const struct Position* pos, struct SearchData* sd, const Move move, int bonus);
+// Getters for the history heuristics
+int GetHHScore(const struct Position* pos, const struct SearchData* sd, const Move move);
+int GetCHScore(const struct SearchStack* ss, const Move move);
+int GetSingleCHScore(const struct SearchStack* ss, const Move move, const int offset);
+int GetCapthistScore(const struct Position* pos, const struct SearchData* sd, const Move move);
+int GetHistoryScore(const struct Position* pos, const struct SearchData* sd, const Move move, const struct SearchStack* ss);
+// Clean all the history tables
+void CleanHistories(struct SearchData* sd);
+// Updates history heuristics for a single move
+void updateHHScore(const struct Position* pos, struct SearchData* sd, const Move move, int bonus);
+void updateCHScore(struct SearchStack* ss, const Move move, const int bonus);
+void updateCapthistScore(const struct Position* pos, struct SearchData* sd, const Move move, int bonus);
 
-    // Corrhist stuff
-    void updateCorrHistScore(const struct Position* pos, struct SearchData* sd, const struct SearchStack* ss, const int depth, const int diff);
-    int  adjustEvalWithCorrHist(const struct Position* pos, const struct SearchData* sd, const int rawEval);
-#ifdef __cplusplus
-}
-#endif
+// Corrhist stuff
+void updateCorrHistScore(const struct Position* pos, struct SearchData* sd, const struct SearchStack* ss, const int depth, const int diff);
+int  adjustEvalWithCorrHist(const struct Position* pos, const struct SearchData* sd, const int rawEval);
