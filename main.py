844675e31a3f71e219cdacb4f9d58b70c957b1e9
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-12-12 12:53:32 +0100

    make double sure the thing stops

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def chess_bot(obs):
         p=subprocess.Popen(f,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=1)
     t=int(obs.remainingOverageTime*1000)
     # Treat delay as inc
+    p.stdin.write(f"stop\n")
+    p.stdin.flush()
     p.stdin.write(f"position fen {obs.board}\ngo wtime {t} btime {t} winc 80 binc 80\n")
     p.stdin.flush()
     line = ""
