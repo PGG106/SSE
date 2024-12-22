f282d136a81771586a807bff5a871f31942d70f0
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2024-12-22 22:10:06 +0200

    simplify

--- src/threads.c
+++ src/threads.c
@@ -2,16 +2,12 @@
 
 #include "shims.h"
 
-// If needed, define SIGCHLD for your architecture:
 #define SIGCHLD 17
-
-// Linux clone flags you want (example):
 #define CLONE_VM      0x00000100
 #define CLONE_FS      0x00000200
 #define CLONE_FILES   0x00000400
 #define CLONE_SIGHAND 0x00000800
 
-// Example combine them:
 #define MY_THREAD_FLAGS (CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | SIGCHLD)
 
 // We define a new helper that can take 5 parameters plus the syscall number (6 total).
@@ -36,36 +32,22 @@ ssize_t _sys6(ssize_t call,
     return ret;
 }
 
-// Minimal clone wrapper for demonstration:
-static ssize_t clone_syscall(ssize_t flags, ssize_t stack,
-    ssize_t ptid, ssize_t ctid, ssize_t tls)
-{
-    // Syscall #56 is clone on x86_64.
-    return _sys6(56, flags, stack, ptid, ctid, tls);
-}
-
 int createThread(thread_fn fn, void* arg)
 {
-    // Allocate minimal stack:
     const size_t STACK_SIZE = 8192;
     char* stackMem = (char*)malloc(STACK_SIZE);
     if (!stackMem) {
         return -1;
     }
 
-    // For downward-growing stack, pass the end address:
     char* stackTop = stackMem + STACK_SIZE;
 
     // Do the clone. 
-    int ret = (int)clone_syscall(MY_THREAD_FLAGS, (ssize_t)stackTop, 0, 0, 0);
+    int ret = (int)_sys6(56, MY_THREAD_FLAGS, (ssize_t)stackTop, 0, 0, 0);
     if (ret == 0)
     {
-        // Child path (clone returns 0 here).
         fn(arg);
-        // If you used CLONE_THREAD plus other flags, you might want:
-        //   _sys(231, 0, 0, 0); // SYS_exit for just this thread
-        // If you used “thread-like” flags incorrectly, exit(0) might kill the process. 
         exit(0);
     }
-    return ret;  // Parent gets child's TID, or error.
+    return ret;
 }
