7441dc1820040872f98bec383a32e1a17dcf5926
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-01-09 04:02:08 +0200

    test
    Bench 5844361

--- src/main.c
+++ src/main.c
@@ -24,24 +24,26 @@ SMALL int main(int argc, char **argv) {
     } else {
 #endif
 #ifdef OB
-        // assume correct input for private engines
-        char input[1024] = { 0 };
-        for (int i = 0;; i++) {
-            input[i] = argv[1][i];
-            if (argv[1][i] == 0) {
-                break;
+        if (argc > 1) {
+            // assume correct input for private engines
+            char input[1024] = { 0 };
+            for (int i = 0;; i++) {
+                input[i] = argv[1][i];
+                if (argv[1][i] == 0) {
+                    break;
+                }
             }
+            char token[1024];
+            int input_index = 0;
+            next_token(input, &input_index, token); // setoption
+            next_token(input, &input_index, token); // name
+            next_token(input, &input_index, token); // EvalFile
+            next_token(input, &input_index, token); // value
+            next_token(input, &input_index, token);
+            NNUE_init(token);
+            StartBench(14);
+            exit(0);
         }
-        char token[1024];
-        int input_index = 0;
-        next_token(input, &input_index, token); // setoption
-        next_token(input, &input_index, token); // name
-        next_token(input, &input_index, token); // EvalFile
-        next_token(input, &input_index, token); // value
-        next_token(input, &input_index, token);
-        NNUE_init(token);
-        StartBench(14);
-        exit(0);
 #endif
         UciLoop();
 #ifdef UCI
