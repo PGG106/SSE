fb3ccf1a2956527795effe1339573685c9671f27
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-01-09 03:50:34 +0200

    test

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "init.h"
 #include "bench.h"
 #include "uci.h"
+#include "io.h"
 
 #ifdef NOSTDLIB
 SMALL __attribute__((naked)) void _start() {
@@ -21,6 +22,24 @@ SMALL int main(int argc, char **argv) {
     if (bench) {
         StartBench(14);
     } else {
+#endif
+#ifdef OB
+        // assume correct input for private engines
+        char input[1024] = { 0 };
+        for (int i = 0;; i++) {
+            input[i] = argv[1][i];
+            if (argv[1][i] == 0) {
+                break;
+            }
+        }
+        char token[1024];
+        int input_index = 0;
+        next_token(input, &input_index, token); // setoption
+        next_token(input, &input_index, token); // name
+        next_token(input, &input_index, token); // EvalFile
+        next_token(input, &input_index, token); // value
+        next_token(input, &input_index, token);
+        NNUE_init(token);
 #endif
         UciLoop();
 #ifdef UCI
