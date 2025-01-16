388bbcf7ce3a51cd20e21890a39a6c2927c16d3c
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-01-16 13:40:45 +0100

    adapt main.py Bench: 5878007

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-import subprocess,os,lzma
-p=None
+import subprocess, os, lzma
 
-f="./nn.net" if os.path.exists("./nn.net") else "/kaggle_simulations/agent/nn.net"
+p = None
+pondermove = None
+
+f = "./nn.net" if os.path.exists("./nn.net") else "/kaggle_simulations/agent/nn.net"
 with lzma.open(f"{f}.xz", 'rb') as c:
-    d=c.read()
+    d = c.read()
 with open(f, 'wb') as o:
     o.write(d)
 
-f="./sse" if os.path.exists("./sse") else "/kaggle_simulations/agent/sse"
+f = "./sse" if os.path.exists("./sse") else "/kaggle_simulations/agent/sse"
 with lzma.open(f"{f}.xz", 'rb') as c:
-    d=c.read()
+    d = c.read()
 with open(f, 'wb') as o:
     o.write(d)
 os.chmod(f, 0o777)
 
+
 def chess_bot(b):
     global p
-    if not p or p.poll()!=None:
-        p=subprocess.Popen(f,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=1)
-    t=int(b.remainingOverageTime*1000)
+    global pondermove
+    if not p or p.poll() != None:
+        p = subprocess.Popen(f, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=1)
+    t = int(b.remainingOverageTime * 1000)
     # Treat delay as inc
+    if b.opponentMove == pondermove:
+        p.stdin.write(f"ponderhit\n")
+    else:
+        p.stdin.write(f"pondermiss\n")
     p.stdin.write(f"position fen {b.board}\ngo wtime {t} btime {t} winc 0 binc 0\n")
     p.stdin.flush()
     l = ""
@@ -27,4 +35,5 @@ def chess_bot(b):
         l = p.stdout.readline()
         if l != "\n":
             break
+    pondermove = l.split()[-1]
     return l.split()[-3]
