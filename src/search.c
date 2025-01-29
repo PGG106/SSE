1eeba807f598f9af0bc886baa874aebde753416a
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-01-29 03:42:27 +0100

    restore table usage Bench: 9128078

--- src/search.c
+++ src/search.c
@@ -366,13 +366,11 @@ SMALL int AspirationWindowSearch(int prev_eval, int depth, struct ThreadData* td
         if (score <= alpha) {
             beta = (alpha + beta) / 2;
             alpha = max(-MAXSCORE, score - delta);
-            depth = td->RootDepth;
         }
 
         // We fell outside the window, so try again with a bigger window
         else if (score >= beta) {
             beta = min(score + delta, MAXSCORE);
-            depth = max(depth - 1, td->RootDepth - 5);
         }
         else
             break;
